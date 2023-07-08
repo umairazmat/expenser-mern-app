fcae05fde903990640bb1da3f4840abebf2fb334
Umair Azmat <umairazmat8260.com>
2023-07-08 19:05:38 +0500

    List Improved 🔥

--- client/src/components/TransactionsList.js
+++ client/src/components/TransactionsList.js
@@ -10,6 +10,7 @@ import { Typography } from '@mui/material';
 
 
 export default function TransactionsList({transactions}) {
+    let rowNumber = 1;
   return (
     <>
     <Typography variant='h6' sx={{marginTop: "20px" }}>Transactions List</Typography>
@@ -17,10 +18,12 @@ export default function TransactionsList({transactions}) {
       <Table sx={{ minWidth: 650 }} size="medium" aria-label="a dense table">
         <TableHead>
           <TableRow>
-            <TableCell>Amount</TableCell>
-            <TableCell align="right">Title</TableCell>
-            <TableCell align="right">Description</TableCell>
-            <TableCell align="right">Date</TableCell>
+           <TableCell align="center">No.</TableCell>
+            <TableCell align="center">Amount</TableCell>
+            <TableCell align="center">Title</TableCell>
+            <TableCell align="center">Description</TableCell>
+            <TableCell align="center">Date</TableCell>
+            <TableCell align="center">Actions</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -29,10 +32,12 @@ export default function TransactionsList({transactions}) {
               key={trx._id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">{trx.amount}</TableCell>
-              <TableCell align="right">{trx.title}</TableCell>
-              <TableCell align="right">{trx.description}</TableCell>
-              <TableCell align="right">{trx.date}</TableCell>
+            <TableCell align="center">{rowNumber++}</TableCell>
+              <TableCell component="th" scope="row"  align="center">{trx.amount}</TableCell>
+              <TableCell align="center">{trx.title}</TableCell>
+              <TableCell align="center">{trx.description}</TableCell>
+              <TableCell align="center">{trx.date}</TableCell>
+              <TableCell align="center">edit    delete</TableCell>
             </TableRow>
           ))}
         </TableBody>
