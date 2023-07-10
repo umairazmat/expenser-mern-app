a1910db1e760be663848451bb241d75ce1735869
Umair Azmat <umairazmat8260.com>
2023-07-11 00:44:44 +0500

    Logo & Fav Icon Updated ❤️

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -5,7 +5,8 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import walletIcon from '../assets/icons/walletIcon.png';
+
 
 export default function ButtonAppBar() {
   return (
@@ -13,13 +14,13 @@ export default function ButtonAppBar() {
       <AppBar position="static">
         <Toolbar>
           <IconButton
-            size="large"
+            size="small"
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
+            sx={{ mr: 1  , padding: "0px"}}
           >
-            <MenuIcon />
+           <img src={walletIcon} alt="Wallet Icon" style={{ width: "30px", height: "30px" }} />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Expenser App
