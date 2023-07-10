38c5ad38a4742de173ae0e6b7b377f138939ba6d
Umair Azmat <umairazmat8260.com>
2023-07-11 02:20:26 +0500

    Login Added

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -1,38 +1,68 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import walletIcon from '../assets/icons/walletIcon.png';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import walletIcon from "../assets/icons/walletIcon.png";
+import { Link } from "react-router-dom";
 
 export default function ButtonAppBar() {
   return (
-    <Box sx={{ flexGrow: 1, margin: '0px', padding: '0px' }}>
+    <Box sx={{ flexGrow: 1, margin: "0px", padding: "0px" }}>
       <AppBar position="static">
-        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Link
+            to="/"
+            style={{
+              textDecoration: "none",
+              color: "white",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
             <IconButton
               size="small"
               edge="start"
               color="inherit"
               aria-label="menu"
-              sx={{ mr: 1, padding: '0px' }}
+              sx={{ marginRight: 1, padding: "0px" }}
             >
-              <img src={walletIcon} alt="Wallet Icon" style={{ width: '30px', height: '30px' }} />
+              <img
+                src={walletIcon}
+                alt="Wallet Icon"
+                style={{ width: "30px", height: "30px" }}
+              />
             </IconButton>
-            
-          </Link>
-          <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
-          <Typography variant="h6" component="div" sx={{ textDecoration: 'none', color: 'inherit' }}>
+            <Typography
+              variant="h6"
+              sx={{ textDecoration: "none", color: "inherit", marginLeft: 1 }}
+            >
               Expenser App
             </Typography>
-            </Link>
-          <Link to="/login" style={{ textDecoration: 'none', color: 'white', textAlign: 'left' }}>
-            <Button color="inherit">Login</Button>
           </Link>
+
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Link
+              to="/login"
+              style={{ textDecoration: "none", color: "white", marginRight: 1 }}
+            >
+              <Button color="inherit">Login</Button>
+            </Link>
+            <Link
+              to="/register"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              <Button color="inherit">Register</Button>
+            </Link>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
