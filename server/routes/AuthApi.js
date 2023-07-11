4c5ae329dac433ef475aceef6b0ba66ffb9aabd1
Umair Azmat <umairazmat8260.com>
2023-07-11 06:49:38 +0500

    Register User done 🚀

--- server/routes/AuthApi.js
+++ server/routes/AuthApi.js
@@ -1,19 +1,38 @@
 import { Router } from 'express';
 const router = Router();
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
   // getting user from server
   router.post("/register", async (req, res) => {
     try {
-    //   const { amount, title, description, date } = req.body;
-    //   const transaction = new TransactionModel({
-    //     amount,
-    //     title,
-    //     description,
-    //     date
-    //   });
+
+      // Check if user is already registered
+      console.log(req.body);
+      const { email , password , firstName, lastName} = req.body;
+
+      const userExists = await User.findOne({ email });
+
+      if (userExists) {
+         res.status(400).json({ error: "User already exists" });
+         return;
+      }
+     
+      // Hash the password
+      const saltRounds = 10;
+      const salt = await bcrypt.genSaltSync(saltRounds);
+       const hash = await  bcrypt.hashSync(password, salt);
   
-    //   await transaction.save(); // Saving data in the database
-      res.json({ message: "New User Created" });
+     // Saving data in the database
+     
+      const newUser = new User({
+        firstName,
+        lastName,
+        email,
+        password: hash,
+      });
+      await newUser.save();
+      res.status(201).json({ message: "New User is Created" });
     } catch (error) {
       // Handle the error appropriately
       res.status(500).json({ error: "Internal Server Error" });
