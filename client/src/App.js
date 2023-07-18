8b08c7afad8bc2bd2d9da2e2a9fc0c784fcd094a
Umair Azmat <umairazmat8260.com>
2023-07-19 01:46:42 +0500

    redux-store setup done 🚀

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import AppBar from './components/AppBar';
 import { Outlet } from "react-router-dom";
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "./store/auth.js";
 
 function App() {
+  
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, []);
+
+  console.log(auth);
   
   return (
     <>
      <AppBar/>
      <Outlet />
-   
     </>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import routes from "./routes"; // Import the 'routes' from the separate file (routes.js)
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import router from "./routes.js";
+import store from "./store/index.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-     <RouterProvider router={routes} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
